cd5e49e99fd169818fd606b44c12f4ca082fad6e
Jessica Alencar <jessicacontatoalencar@gmail.com>
2023-11-16 16:24:01 -0300

    Get lists

---
+++ src/main/java/com/devsuperior/dslist/repositories/GameListRepository2.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dslist.entities.GameList;
+
+public interface GameListRepository2 extends JpaRepository<GameList, Long> {
+	
+	
+	
+
+}
